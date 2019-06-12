9dba7056658fb096b96eb9050c737b627d71589c
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-12 22:38:53 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,6 +4,9 @@ def hello_t(array)
   while i < array.length 
     yield array[i]   
     i = i + 1 
+  hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
   end
 end
 
